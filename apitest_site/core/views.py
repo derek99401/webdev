1e7c745afdbe3195af84345a6b747e996971694c
Derek Ma <derekxm@eyestalks.com>
2014-08-01 18:34:13 -0700

    check params for views

--- apitest_site/core/views.py
+++ apitest_site/core/views.py
@@ -3,6 +3,7 @@ from django.views.generic import View
 from django.http import HttpResponse
 from rest_framework.parsers import FileUploadParser
 from rest_framework.views import APIView
+import hashlib, os
 
 
 # Create your views here.
@@ -15,10 +16,27 @@ class FileUploadView(APIView):
 
   def post(self, request):
     file_obj = request.FILES['file']
-    upload_file(file_obj)
+    if not verify_params(self, request):
+      return HttpResponse(content = 'bad request', status = 400)
+    upload_file(file_obj, filename = request.POST['filename'], placement_name = request.POST['id'], md5 = request.POST['md5'])
     return HttpResponse(content = 'OK', status = 200)
 
-def upload_file(file_obj):
-  with open('/tmp/tmpfile', 'wb+') as dest_file:
+  def verify_params(self, request):
+    if 'id' not in request.POST:
+      return False
+    if 'md5' not in request.POST:
+      return False
+    if 'filename' not in request.POST:
+      return False
+    return True
+
+def upload_file(file_obj, filename = None, placement_name = None, md5 = None):
+  md5_hasher = hashlib.md5()
+  with open(os.path.join('/tmp/',filename), 'wb+') as dest_file:
     for chunk in file_obj.chunks():
       dest_file.write(chunk)
+      md5_hasher.update(chunk)
+  if md5_hasher.hexdigest != md5:
+    return False
+
+  return True
